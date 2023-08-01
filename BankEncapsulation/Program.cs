810f139e5ccd817029bab5efdb6654f063ab4347
Cali Weber <calista.weber1@gmail.com>
2023-08-01 12:14:57 -0700

    finished exercise

--- BankEncapsulation/Program.cs
+++ BankEncapsulation/Program.cs
@@ -1,10 +1,27 @@
-﻿namespace BankEncapsulation
+﻿using System.Dynamic;
+
+namespace BankEncapsulation
 {
     public class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            var Account = new BankAccount();
+
+            var Accountbalance = Account.GetBalance();
+
+            Console.WriteLine($"Your current balance is {Accountbalance}");
+
+            Console.WriteLine("How much would you like to add?");
+
+
+            Account.Deposit(double.Parse(Console.ReadLine()));
+
+            Accountbalance = Account.GetBalance();
+
+            Console.WriteLine($"Your new balance is {Accountbalance}");
+
+
         }
     }
 }
